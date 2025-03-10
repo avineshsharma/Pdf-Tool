431c31bb1be13ee46eab9ba9bf8f65dca57fa5e9
avineshsharma <jangraavinesh@gmail.com>
2025-03-10 23:09:44 +0530

    Your commit message

--- testpdf.js
+++ testpdf.js
@@ -3,8 +3,8 @@ import PDFMerger from 'pdf-merger-js';
 var merger = new PDFMerger();
 
 (async (p1,p2) => {
-  await merger.add('1.pdf');  //merge all pages. parameter is the path to file and filename.
-  await merger.add('2.pdf'); // merge only page 2
+  await merger.add('11.pdf');  //merge all pages. parameter is the path to file and filename.
+  await merger.add('22.pdf'); // merge only page 2
 
 
 
