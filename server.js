7efeb8812d5c331a08572a2a25d96e3924bfdd83
avineshsharma <jangraavinesh@gmail.com>
2025-03-12 18:32:26 +0530

    error correct

--- server.js
+++ server.js
@@ -1,22 +1,55 @@
+// const express = require('express')
+// const path   = require('path')
+// const app = express()
+// const multer  = require('multer')
+// const mergerpdfs  = require('./testpdf')
+// const upload = multer({ dest: 'uploads/' })
+// app.use('/static',express.static('public'))
+// const port = 300
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname,"templates/index.html"))
+// })
+
+// app.post('/merge', upload.array('pdfs', 2), function (req, res, next) {
+//     console.log(req.files)
+//     mergerpdfs(path.join(__dirname,req.file[0].path), path.join(__dirname,req.file[1].path))
+//     res.redirect("http://localhost:300/static/merged.pdf")
+//     // res.send({data:req.files})
+//     // req.files is array of `photos` files
+//     // req.body will contain the text fields, if there were any
+//   })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port http://localhost:${port}`)
+// })
+
+
+
+
 const express = require('express')
-const path   = require('path')
+const path = require('path')
 const app = express()
 const multer  = require('multer')
-const mergerpdfs  = require('mergepdfs')
+const {mergePdfs}  = require('./merge')
+
 const upload = multer({ dest: 'uploads/' })
-const port = 300
+app.use('/static', express.static('public'))
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,"templates/index.html"))
+  res.sendFile(path.join(__dirname, "templates/index.html"))
 })
 
-app.post('/merge', upload.array('pdfs', 2), function (req, res, next) {
-    console.log(req.files)
-    mergerpdfs(path.join(__dirname,req.file[0].path))
-    res.send({data:req.files})
-    // req.files is array of `photos` files
-    // req.body will contain the text fields, if there were any
-  })
+ 
+
+app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
+  console.log(req.files)
+  let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+  res.redirect(`http://localhost:3000/static/${d}.pdf` )
+  // res.send({data: req.files})
+  // req.files is array of `photos` files
+  // req.body will contain the text fields, if there were any
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
